c92200da4a4e3871a3009cdcef13d032668ac573
achyu <achyut1234duggal@gmail.com>
2024-08-20 11:27:08 +0530

    cart commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@
 import { Routes, Route} from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
-const Shop = () => {
-  return <h1>I am shop element</h1>
-}
+import Shop from './routes/shop/shop.component'
 
 const App = () => {
   return (
@@ -14,7 +12,7 @@ const App = () => {
       <Route path ='/' element ={<Navigation/>}>
         <Route index element = {<Home />}/> // index is the way to match the / route. Both will render together
         <Route path="shop" element ={<Shop />} />
-        <Route path="sign-in" element ={<SignIn />} />
+        <Route path="auth" element ={<Authentication />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,28 +1,45 @@
-import {Link, Outlet} from 'react-router-dom';
-import { Fragment } from 'react';
-import './navigation.styles.scss';
+import { Link, Outlet } from "react-router-dom";
+import { Fragment, useContext } from "react";
+import "./navigation.styles.scss";
 
-import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../context/user.context";
+import { CartContext} from "../../context/cart.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
-    return(
-        <Fragment>
-            <div className='navigation'>
-                <Link className='logo-container' to="/">
-                <CrwnLogo className ='logo'/>
-                </Link>
-                <div className='nav-links-container'>
-                    <Link className='nav-link' to ="/shop"> 
-                    SHOP
-                    </Link>
-                    <Link className='nav-link' to ="/sign-in"> 
-                    SIGN IN
-                    </Link>
-                </div>
-            </div>
-            <Outlet />
-        </Fragment>
-    )
-}
+  const { currentUser } = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext)
+
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
+          <CartIcon />
+        </div>
+        {isCartOpen && <CartDropdown />}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
